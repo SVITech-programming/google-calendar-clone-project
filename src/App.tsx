cc005d29c6098672e229cb3f4147cdf86f302e63
Martin Svitek <martin.svitek92@gmail.com>
2026-07-17 23:33:43 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Calendar } from "./components/Calendar";
+import { EventsProvider } from "./context/Events";
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+      <EventsProvider>
+        <Calendar />
+      </EventsProvider>
+    </>
+  );
+}
+
+export default App;
